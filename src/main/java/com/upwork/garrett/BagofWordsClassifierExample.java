edee96c314218091345161bdd505516c573e7757
GarrethDottin <garrethdottin@gmail.com>
2016-09-11 12:47:27 -0400

    setup n-grams

--- src/main/java/com/upwork/garrett/BagofWordsClassifierExample.java
+++ src/main/java/com/upwork/garrett/BagofWordsClassifierExample.java
@@ -18,6 +18,10 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.ArrayList.*;
+import org.apache.commons.lang3.ArrayUtils;
 import org.deeplearning4j.models.word2vec.wordstore.inmemory.AbstractCache;
 import org.datavec.api.util.ClassPathResource;
 import org.deeplearning4j.models.embeddings.WeightLookupTable;
@@ -35,10 +39,9 @@ import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFac
 import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import edu.stanford.nlp.util.CollectionUtils;
 
-/**
- * Hello world!
- */
+
 public class BagofWordsClassifierExample {
 
     VocabCache vocabCache;
@@ -47,90 +50,53 @@ public class BagofWordsClassifierExample {
     HashMap<String, Integer>  vocabWeights = Constants.VOCAB_WEIGHTS;
     String[] stopWords = Constants.STOP_WORDS;
 
+    private static List<String> splitOne(String wordString) {
+        return Arrays.asList(wordString.split("#"));
+    }
+
+    private static List<List<String>> split(String wordListsString) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        for (String wordString : wordListsString.split(" ")) {
+            result.add(splitOne(wordString));
+        }
+        return result;
+    }
+
     public static void main(String[] args) throws Exception {
         BagofWordsClassifierExample app = new BagofWordsClassifierExample();
+        ClassPathResource resource = new ClassPathResource("Albert_Einstein.txt");
+        InputStream einsteinStrem = resource.getInputStream(); //Thread.currentThread().getContextClassLoader().getResourceAsStream("labeled/muisc/Albert_Einstein.txt");
+        String s = IOUtils.toString(einsteinStrem);
+        List items = split(s);
+        List<List<String>> testing =  CollectionUtils.getNGrams(items, 1,3);
 
-        app.buildTfIdfMatrix();
+        app.weightDoc(testing);
         app.checkUnlabeledData();
 
 
     }
 
-    private AbstractCache createCache() throws IOException {
-        ClassPathResource resource = new ClassPathResource("Albert_Einstein.txt");
-        File file = resource.getFile();
-        AbstractCache<VocabWord> vocabCache =  new AbstractCache.Builder<VocabWord>().build();
-
-        /*
-            First we build line iterator
-         */
-        BasicLineIterator underlyingIterator = new BasicLineIterator(file);
-                /*
-            Now we need the way to convert lines into Sequences of VocabWords.
-            In this example that's SentenceTransformer
-         */
-        TokenizerFactory t = new DefaultTokenizerFactory();
-        t.setTokenPreProcessor(new CommonPreprocessor());
-
-        SentenceTransformer transformer = new SentenceTransformer.Builder()
-                .iterator(underlyingIterator)
-                .tokenizerFactory(t)
-                .build();
-
-
-        /*
-            And we pack that transformer into AbstractSequenceIterator
-         */
-        AbstractSequenceIterator<VocabWord> sequenceIterator =
-                new AbstractSequenceIterator.Builder(transformer).build();
-
-        /*
-            Now we should build vocabulary out of sequence iterator.
-            We can skip this phase, and just set AbstractVectors.resetModel(TRUE), and vocabulary will be mastered internally
-        */
-        VocabConstructor<VocabWord> constructor = new VocabConstructor.Builder<VocabWord>()
-                .addSource(sequenceIterator, 5)
-                .setTargetVocabCache(vocabCache)
-                .build();
-
-        constructor.buildJointVocabulary(false, true);
-
-        return vocabCache;
-
-    }
-
-    private void buildTfIdfMatrix() throws IOException {
-        ClassPathResource resource = new ClassPathResource("Albert_Einstein.txt");
-        File file = resource.getFile();
-
-        iterator = new FileLabelAwareIterator.Builder().addSourceFolder(resource.getFile()).build();
-        tokenizerFactory = new DefaultTokenizerFactory();
-        tokenizerFactory.setTokenPreProcessor(new CommonPreprocessor());
-
-        AbstractCache<VocabWord> vocabCache = createCache();
-
-        BagOfWordsVectorizer bagOfWordsVectorizer = new BagOfWordsVectorizer.Builder().setIterator(iterator).setTokenizerFactory(tokenizerFactory).setStopWords(Arrays.asList(stopWords)).setMinWordFrequency(1).setVocab(vocabCache).build();
-        InputStream einsteinStrem = resource.getInputStream(); //Thread.currentThread().getContextClassLoader().getResourceAsStream("labeled/muisc/Albert_Einstein.txt");
-        String s = IOUtils.toString(einsteinStrem);
-
-        DataSet dataSet = bagOfWordsVectorizer.vectorize(s,"Music");
-
-        System.out.println("dataSet = " + dataSet);
-    }
-
-    public Integer weightDoc (ArrayList<String> dataSet) {
+    public Integer weightDoc (List<List<String>> dataSet) {
         Integer weightedScore = 0;
-        for (String word : dataSet) {
-            weightedScore += setWeight(word);
+        for (List<String> word : dataSet) {
+            String concatenatedWords = Concat(word);
+            weightedScore += setWeight(concatenatedWords);
         }
         return weightedScore;
     }
-    public Integer setWeight(String word) {
+    private String Concat(List<String> word) {
+        String finalizedWord = "";
+        for (int i = 0; i < word.size(); i++)  {
+            finalizedWord +=  word.get(i);
+        }
+
+        return finalizedWord;
+    }
+    private Integer setWeight(String word) {
         Integer indvidualWordWeight = 0;
         for (String key : vocabWeights.keySet() ) {
             if (word.equals(key)){
                 indvidualWordWeight += vocabWeights.get(key);
-                break;
             }
         }
         return indvidualWordWeight;
