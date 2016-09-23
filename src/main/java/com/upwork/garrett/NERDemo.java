f621c5bb3a8a7e3f9961274611adb19b0a9cdba5
GarrethDottin <garrethdottin@gmail.com>
2016-09-23 19:20:45 -0400

    NER WIP

--- src/main/java/com/upwork/garrett/NERDemo.java
+++ src/main/java/com/upwork/garrett/NERDemo.java
@@ -7,10 +7,11 @@ import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.sequences.DocumentReaderAndWriter;
 import edu.stanford.nlp.util.Triple;
-
+import java.util.Set;
+import java.util.Optional;
 import java.util.List;
-
-
+import java.util.ArrayList;
+import java.util.HashMap;
 /** This is a demo of calling CRFClassifier programmatically.
  *  <p>
  *  Usage: {@code java -mx400m -cp "*" NERDemo [serializedClassifier [fileName]] }
@@ -42,7 +43,7 @@ public class NERDemo {
 
         // Create model using props file
         String serializedClassifier = "src/main/classifiers/english.all.3class.distsim.crf.ser.gz";
-
+        ArrayList<String> possibleMusicians = new ArrayList<String>();
         if (args.length > 0) {
             serializedClassifier = args[0];
         }
@@ -55,53 +56,6 @@ public class NERDemo {
        demo file shows several ways to process the input, for teaching purposes.
     */
 
-        if (args.length > 1) {
-
-      /* For the file, it shows (1) how to run NER on a String, (2) how
-         to get the entities in the String with character offsets, and
-         (3) how to run NER on a whole file (without loading it into a String).
-      */
-
-            String fileContents = IOUtils.slurpFile(args[1]);
-            List<List<CoreLabel>> out = classifier.classify(fileContents);
-            for (List<CoreLabel> sentence : out) {
-                for (CoreLabel word : sentence) {
-                    System.out.print(word.word() + '/' + word.get(CoreAnnotations.AnswerAnnotation.class) + ' ');
-                }
-                System.out.println();
-            }
-
-            System.out.println("---");
-            out = classifier.classifyFile(args[1]);
-            for (List<CoreLabel> sentence : out) {
-                for (CoreLabel word : sentence) {
-                    System.out.print(word.word() + '/' + word.get(CoreAnnotations.AnswerAnnotation.class) + ' ');
-                }
-                System.out.println();
-            }
-
-            System.out.println("---");
-            List<Triple<String, Integer, Integer>> list = classifier.classifyToCharacterOffsets(fileContents);
-            for (Triple<String, Integer, Integer> item : list) {
-                System.out.println(item.first() + ": " + fileContents.substring(item.second(), item.third()));
-            }
-            System.out.println("---");
-            System.out.println("Ten best entity labelings");
-            DocumentReaderAndWriter<CoreLabel> readerAndWriter = classifier.makePlainTextReaderAndWriter();
-            classifier.classifyAndWriteAnswersKBest(args[1], 10, readerAndWriter);
-
-            System.out.println("---");
-            System.out.println("Per-token marginalized probabilities");
-            classifier.printProbs(args[1], readerAndWriter);
-
-            // -- This code prints out the first order (token pair) clique probabilities.
-            // -- But that output is a bit overwhelming, so we leave it commented out by default.
-            // System.out.println("---");
-            // System.out.println("First Order Clique Probabilities");
-            // ((CRFClassifier) classifier).printFirstOrderProbs(args[1], readerAndWriter);
-
-        } else {
-
       /* For the hard-coded String, it shows how to run it on a single
          sentence, and how to do this and produce several formats, including
          slash tags and an inline XML output format. It also shows the full
@@ -109,63 +63,27 @@ public class NERDemo {
          classifier. And it shows getting out the probabilities of different
          assignments and an n-best list of classifications with probabilities.
       */
-
+            HashMap<String, String[]> sampleText = new HashMap<>();
             String[] example = {"Good afternoon Rajat Raina, how are you today?",
                     "I go to school at Stanford University, which is located in California." };
-            for (String str : example) {
-                System.out.println(classifier.classifyToString(str));
-            }
-            System.out.println("---");
+            sampleText.put("Einstein", example);
 
-            for (String str : example) {
-                // This one puts in spaces and newlines between tokens, so just print not println.
-                System.out.print(classifier.classifyToString(str, "slashTags", false));
-            }
-            System.out.println("---");
+            String Scientist = sampleText.keySet().stream().findFirst().toString();
 
-            for (String str : example) {
-                // This one is best for dealing with the output as a TSV (tab-separated column) file.
-                // The first column gives entities, the second their classes, and the third the remaining text in a document
-                System.out.print(classifier.classifyToString(str, "tabbedEntities", false));
-            }
-            System.out.println("---");
-
-            for (String str : example) {
-                System.out.println(classifier.classifyWithInlineXML(str));
-            }
-            System.out.println("---");
-
-            for (String str : example) {
-                System.out.println(classifier.classifyToString(str, "xml", true));
-            }
-            System.out.println("---");
-
-            for (String str : example) {
-                System.out.print(classifier.classifyToString(str, "tsv", false));
-            }
-            System.out.println("---");
-
-            // This gets out entities with character offsets
-            int j = 0;
-            for (String str : example) {
-                j++;
-                List<Triple<String,Integer,Integer>> triples = classifier.classifyToCharacterOffsets(str);
-                for (Triple<String,Integer,Integer> trip : triples) {
-                    System.out.printf("%s over character offsets [%d, %d) in sentence %d.%n",
-                            trip.first(), trip.second(), trip.third, j);
-                }
-            }
-            System.out.println("---");
-
-            // This prints out all the details of what is stored for each token
-            int i=0;
-            /*  Interesting piece of code to use for each token
-            */
-
-            for (String str : example) {
+            for (String str : sampleText.get(Scientist)) {
                 for (List<CoreLabel> lcl : classifier.classify(str)) {
                     for (CoreLabel cl : lcl) {
-                        System.out.print(i++ + ": ");
+                        // Check if the value matches Music
+                        //
+
+                        String entityDetails = cl.toShortString();
+                        String entityType = entityDetails.replace("^[^_]*Answer=","");
+
+                        if ("Cool".equals("Music")) {
+                            possibleMusicians.add(Scientist);
+                            break;
+                        }
+                        // if it does break make a note about that document
                         System.out.println(cl.toShorterString());
                     }
                 }
@@ -173,7 +91,6 @@ public class NERDemo {
 
             System.out.println("---");
 
-        }
     }
 
 }
